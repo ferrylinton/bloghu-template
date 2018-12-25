dcb582d95a2a255b13d7d96bf262f45209518cd0
Ferry L. H <v-ferry.linton@btpn.com>
2018-12-25 16:38:01 +0700

    update dummy

--- src/static/js/dummy.js
+++ src/static/js/dummy.js
@@ -9,10 +9,28 @@ var Fragment = (function () {
     }
 
     function init() {
-        load($('#navbar'), '../../fragments/navbar.html nav');
-        load($('#category'), '../../fragments/category.html .box');
-        load($('#post'), '../../fragments/post.html .box');
-        load($('#search'), '../../fragments/search.html form');
+        // load($('#navbar'), '../../fragments/navbar.html nav');
+        // load($('#category'), '../../fragments/category.html .box');
+        // load($('#post'), '../../fragments/post.html .box');
+        // load($('#search'), '../../fragments/search.html form');
+        $('[th\\:replace]').each(function (index, value) { 
+            var tag = $(this).prop('localName')
+            var thReplaceAttr = $(this).attr('th:replace').split('::');
+            var template = '/' + thReplaceAttr[0].trim();
+
+            if(tag == 'xxxxhead'){
+                $(this).load(template, function (responseText, textStatus, jqXHR) {
+                    console.log(`load ${template} is ${textStatus}`);
+                    $('.box-content').find('th\\:block').hide();
+                });
+            }else if(tag == 'div'){
+                $(this).load(template + ' div:first', function (responseText, textStatus, jqXHR) {
+                    console.log(`load ${template} is ${textStatus}`);
+                    $('.box-content').find('th\\:block').hide();
+                });
+            }
+
+        });
     }
 
     return {
@@ -24,5 +42,14 @@ var Fragment = (function () {
 $(function () {
 
     Fragment.init();
+
+
+    // $('[th\\:replace]').each(function (index, value) { 
+    //     console.log($(this));
+    //     console.log($(this).prop('localName'));
+    //     var thReplaceAttr = $(this).attr('th:replace').split('::');
+    //     console.log(thReplaceAttr[0].trim());
+    //    // $(this).html(thReplaceAttr[0].trim());
+    // });
 
 });
